c3e409958f4f32b288b69f7ab2af6bfd639e2a76
michalpolko <michalpolko@o2.pl>
2011-04-08 23:10:32 +0200

    [a656] Update achivement criterias after processing CreatureAI::JustDied() for criteria type: kill creature (for criteria requirements: instance script requirement).

--- DB_updates/mangos/instances/instance_Ulduar.sql
+++ DB_updates/mangos/instances/instance_Ulduar.sql
@@ -144,7 +144,7 @@ INSERT INTO `reference_loot_template` VALUES
 
 -- ****** Razorscale ******
 UPDATE creature_template SET `ScriptName`="boss_razorscale" WHERE entry=33186;
-UPDATE creature_template SET mechanic_immune_mask=617299803, `lootid` = `entry`, `InhabitType` = 7 WHERE entry IN (33186, 33724);
+UPDATE creature_template SET faction_A = 2150, faction_H = 2150, mechanic_immune_mask=617299803, `lootid` = `entry`, `InhabitType` = 7 WHERE entry IN (33186, 33724);
 -- original x=587.547, y= -174.927, z = 391.517; make the boss fly before encounter starts
 update creature set position_x = 590.346741, position_y = -226.947647, position_z = 460.897583 where id = 33186;
 UPDATE gameobject_template SET flags= 6553648, ScriptName="go_broken_harpoon" WHERE entry = 194565;
@@ -372,7 +372,7 @@ INSERT INTO `reference_loot_template` VALUES
 -- arms are mounted on the boss
 DELETE FROM creature WHERE id IN (32933, 32934);
 -- set proper Kologarn position
-UPDATE creature SET position_x = 1797.15, position_y = -24.4027, position_z = 448.741 WHERE id = 32930;
+UPDATE creature SET position_x = 1797.150024, position_y = -24.4027, position_z = 448.740997 WHERE id = 32930;
 
 UPDATE creature_model_info SET bounding_radius = 40, combat_reach = 40 WHERE modelid IN (28638, 28822, 28821);
 UPDATE creature_template SET scriptname='boss_kologarn' WHERE entry = 32930;
@@ -679,3 +679,60 @@ INSERT INTO reference_loot_template VALUES
 UPDATE gameobject SET spawntimesecs = -604800 WHERE id IN (194200, 194201);
 UPDATE gameobject SET spawnMask = 1 WHERE id = 194200;
 UPDATE gameobject SET spawnMask = 2 WHERE id = 194201;
+
+-- achievements
+DELETE FROM `achievement_criteria_requirement` WHERE `criteria_id` IN
+(10062, 10063, 10066, 10067, 10430, 10431, 10068, 10069, 10074, 10075, 10077, 10079, 10221, 10220, 10082, 10085, 10083,
+10086, 10084, 10087, 10088, 10418, 10419, 10089, 10420, 10421, 10090, 10422, 10423, 10091, 10424, 10425, 10285, 10095,
+10286, 10099, 10290, 10133, 10400, 10184, 10399, 10243);
+INSERT INTO `achievement_criteria_requirement` VALUES
+(10062, 18, 0, 0),
+(10063, 18, 0, 0),
+(10066, 18, 0, 0),
+(10067, 18, 0, 0),
+(10430, 12, 0, 0),
+(10430, 6, 4654, 0),
+(10431, 12, 1, 0),
+(10431, 6, 4654, 0),
+(10068, 18, 0, 0),
+(10069, 18, 0, 0),
+(10074, 18, 0, 0),
+(10075, 18, 0, 0),
+(10077, 18, 0, 0),
+(10079, 18, 0, 0),
+(10221, 7, 65737, 0),
+(10220, 7, 64193, 0),
+(10082, 18, 0, 0),
+(10085, 18, 0, 0),
+(10083, 18, 0, 0),
+(10086, 18, 0, 0),
+(10084, 18, 0, 0),
+(10087, 18, 0, 0),
+(10088, 18, 0, 0),
+(10418, 18, 0, 0),
+(10419, 18, 0, 0),
+(10089, 18, 0, 0),
+(10420, 18, 0, 0),
+(10421, 18, 0, 0),
+(10088, 5, 58501, 0),
+(10418, 5, 58501, 0),
+(10419, 5, 58501, 0),
+(10089, 5, 58501, 0),
+(10420, 5, 58501, 0),
+(10421, 5, 58501, 0),
+(10090, 18, 0, 0),
+(10422, 18, 0, 0),
+(10423, 18, 0, 0),
+(10091, 18, 0, 0),
+(10424, 18, 0, 0),
+(10425, 18, 0, 0),
+(10285, 18, 0, 0),
+(10095, 18, 0, 0),
+(10286, 18, 0, 0),
+(10099, 18, 0, 0),
+(10290, 18, 0, 0),
+(10133, 18, 0, 0),
+(10400, 18, 0, 0),
+(10184, 18, 0, 0),
+(10399, 18, 0, 0),
+(10243, 18, 0, 0);
