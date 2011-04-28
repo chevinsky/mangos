72dc5634eaeffecac808ef1dc3aa25e4ba9fef89
michalpolko <michalpolko@getmangos.com>
2011-04-28 17:58:11 +0200

    [a29] Support for spell supercharge (iron council) and  Shield of Runes

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -2264,6 +2264,26 @@ void Spell::SetTargetMap(SpellEffectIndex effIndex, uint32 targetMode, UnitList&
                 FillAreaTargets(targetUnitMap, m_caster->GetPositionX(), m_caster->GetPositionY(), radius, PUSH_DEST_CENTER, SPELL_TARGETS_ALL);
             }
 
+            // Supercharge (Iron Council: Ulduar)
+            if (m_spellInfo->Id == 61920)
+            {
+                std::list<Unit*> tempTargetUnitMap;
+                targetUnitMap.clear();
+                FillAreaTargets(tempTargetUnitMap, m_caster->GetPositionX(), m_caster->GetPositionY(), radius, PUSH_DEST_CENTER, SPELL_TARGETS_NOT_HOSTILE);
+
+                for (std::list<Unit*>::iterator itr = tempTargetUnitMap.begin(),next; itr != tempTargetUnitMap.end(); itr++)
+                {
+                    if ((*itr) &&
+                        ((*itr)->GetEntry() == 32867 || // Steelbreaker
+                        (*itr)->GetEntry() == 32927 || // Runemaster Molgeim
+                        (*itr)->GetEntry() == 32857) // Stormcaller Brundir
+                        )
+                        targetUnitMap.push_back(*itr);
+                }
+
+                return;
+            }
+
             // exclude caster
             targetUnitMap.remove(m_caster);
             break;
