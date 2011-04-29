77011220378881427eaf9298280ba6017c696003
michalpolko <michalpolko@getmangos.com>
2011-04-29 17:27:57 +0200

    [a40] Hack-fixed Chaos Bolt immunity to deflection.

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -3450,6 +3450,9 @@ SpellMissInfo Unit::MagicSpellHitResult(Unit *pVictim, SpellEntry const *spell)
     {
         int32 deflect_chance = pVictim->GetTotalAuraModifier(SPELL_AURA_DEFLECT_SPELLS)*100;
         tmp+=deflect_chance;
+        // Chaos Bolt cannot be deflected
+        if (spell->SpellFamilyName == SPELLFAMILY_WARLOCK && spell->SpellIconID == 3178)
+            return SPELL_MISS_NONE;
         if (rand < tmp)
             return SPELL_MISS_DEFLECT;
     }
