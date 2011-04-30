1b3330c3f8ed75543ec6253468a46b2eb641e208
Grandres <grundil@wp.pl>
2011-04-30 13:48:26 +0200

    Compilation fix

--- src/game/Creature.h
+++ src/game/Creature.h
@@ -52,6 +52,7 @@ enum CreatureFlagsExtra
     CREATURE_FLAG_EXTRA_INVISIBLE       = 0x00000080,       // creature is always invisible for player (mostly trigger creatures)
     CREATURE_FLAG_EXTRA_NOT_TAUNTABLE   = 0x00000100,       // creature is immune to taunt auras and effect attack me
     CREATURE_FLAG_EXTRA_AGGRO_ZONE      = 0x00000200,       // creature sets itself in combat with zone on aggro
+    CREATURE_FLAG_PARTIAL_XP_REWARDED   = 0x00000800,       // creature kill provide 1/8 of normal XP
     CREATURE_FLAG_EXTRA_KEEP_AI         = 0x00001000,       // creature keeps ScriptedAI even after being charmed / controlled (instead of getting PetAI)
 };
 
