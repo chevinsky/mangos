f6d3488aa7540254611c1b3a602197cffd28b772
Grandres <grundil@wp.pl>
2011-05-15 14:19:39 +0200

    [a65] do not allow enter instance when encounter in progress

--- src/game/MapManager.cpp
+++ src/game/MapManager.cpp
@@ -23,6 +23,7 @@
 #include "Log.h"
 #include "Transports.h"
 #include "GridDefines.h"
+#include "InstanceData.h"
 #include "DestinationHolderImp.h"
 #include "World.h"
 #include "CellImpl.h"
@@ -199,15 +200,17 @@ bool MapManager::CanPlayerEnter(uint32 mapid, Player* player)
             return false;
         }
 
-        // TODO: move this to a map dependent location
-        /*if(i_data && i_data->IsEncounterInProgress())
+        if (!player->isGameMaster())
         {
-            DEBUG_LOG("MAP: Player '%s' can't enter instance '%s' while an encounter is in progress.", player->GetName(), GetMapName());
-            player->SendTransferAborted(GetId(), TRANSFER_ABORT_ZONE_IN_COMBAT);
-            return(false);
-        }*/
-    }
+            InstanceData* i_data = ((DungeonMap*)CreateMap(mapid, player))->GetInstanceData();
 
+            if (i_data && i_data->IsEncounterInProgress())
+            {
+                player->SendTransferAborted(mapid, TRANSFER_ABORT_ZONE_IN_COMBAT);
+                return false;
+            }
+        }
+    }
     return true;
 }
 
